5c742ebd73eefbaf6af787f21a4a14ff4239fa9b
Brandon McDonald <bamcd0313@gmail.com>
2023-09-08 08:37:06 -0400

    Update CameraController.cs
    changed to fix style guide

--- Useful Scripts/CameraController.cs
+++ Useful Scripts/CameraController.cs
@@ -2,25 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CameraController : MonoBehaviour
 {
-
-    GameObject player; //player reference
-    Vector3 offset; //distance between player and camera
+    GameObject _player; //player reference
+    Vector3 _offset; //distance between player and camera
 
     // Use this for initialization
     void Start()
     {
-        player = GameObject.Find("Player");
+        _player = GameObject.Find("Player");
 
-        //offset = palyer position - camera position
-        offset = player.transform.position - transform.position;
+        //offset = the difference in player position and camera position
+        _offset = player.transform.position - transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         //camera position = player position - offset
-        transform.position = player.transform.position - offset;
+        transform.position = _player.transform.position - _offset;
     }
 }
