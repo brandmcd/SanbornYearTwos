e5f41b8892bda94194a84e707a90d691c43e915d
UserKorns1202 <kornskids@gmail.com>
2023-09-08 07:36:43 -0400

    Added Basic Camera Controller
    Basic Camera Controller that finds the object names "Player" and follows it in a 2D format

---
+++ Useful Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    GameObject player; //player reference
+    Vector3 offset; //distance between player and camera
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.Find("Player");
+
+        //offset = palyer position - camera position
+        offset = player.transform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //camera position = player position - offset
+        transform.position = player.transform.position - offset;
+    }
+}
